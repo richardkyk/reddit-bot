f964972166f666516d1a97951d5b2e00d4771516
Richard Khaw <28803078+Richardkyk@users.noreply.github.com>
2020-05-01 22:59:58 +1000

    hot fix 4 - added padding for hour

--- bot.js
+++ bot.js
@@ -192,6 +192,7 @@ function convertTime(sec) {
   min >= 1 ? (sec = sec - min * 60) : (min = "00");
   sec < 1 ? (sec = "00") : void 0;
 
+  hours.toString().length == 1 ? (hours = "0" + hours) : void 0;
   min.toString().length == 1 ? (min = "0" + min) : void 0;
   sec.toString().length == 1 ? (sec = "0" + sec) : void 0;
 
--- test.js
+++ test.js
@@ -5,10 +5,11 @@ function convertTime(sec) {
   min >= 1 ? (sec = sec - min * 60) : (min = "00");
   sec < 1 ? (sec = "00") : void 0;
 
+  hours.toString().length == 1 ? (hours = "0" + hours) : void 0;
   min.toString().length == 1 ? (min = "0" + min) : void 0;
   sec.toString().length == 1 ? (sec = "0" + sec) : void 0;
 
   return hours + ":" + min + ":" + sec;
 }
 
-console.log(convertTime(1323000 / 1000));
+console.log(convertTime(~~(32003232 / 1000)));
